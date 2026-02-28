e042a91ce4ee411b64cc58aad26f996114dad715
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-02-28 11:14:04 +0100

    "feat: implement interfaces for doctors and patients following SOLID"

---
+++ src/main/java/org/example/IEmergencyCare.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface IEmergencyCare {
+    void admission();
+    void operation();
+    void room();
+    void treatment();
+    void rehabilitation();
+    void discharge();
+}
---
+++ src/main/java/org/example/IWorkable.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface IWorkable {
+    void workHoursPerWeek();
+    void eat();
+    void vacations();
+}
