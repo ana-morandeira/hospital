885833c837ded689b40a23987ddb597e94700748
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-02 18:18:39 +0100

    feat:AmbulancePatient.java

---
+++ src/main/java/org/example/AmbulancePatient.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class AmbulancePatient extends Patient implements IEmergencyCare {
+    public AmbulancePatient(String id, String firstName, String lastName) { super(id, firstName, lastName); }
+
+    @Override public void admission() { System.out.println("Emergency ER admission."); }
+    @Override public void operation() { System.out.println("Surgery required."); }
+    @Override public void room() { System.out.println("ICU Unit."); }
+    @Override public void treatment() { System.out.println("Emergency treatment."); }
+    @Override public void rehabilitation() { System.out.println("Physical therapy required."); }
+    @Override public void discharge() { System.out.println("Patient stable, discharged."); }
+}
