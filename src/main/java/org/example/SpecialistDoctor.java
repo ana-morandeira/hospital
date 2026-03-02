2d719f283475259a51773c373dae3e670d0d46cf
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-02 18:13:06 +0100

    feat:Patient.java

---
+++ src/main/java/org/example/SpecialistDoctor.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class SpecialistDoctor extends Doctor {
+    private String specialty;
+
+    public SpecialistDoctor(String id, String firstName, String lastName, String specialty) {
+        super(id, firstName, lastName);
+        this.specialty = specialty;
+    }
+
+    @Override public void workHoursPerWeek() { System.out.println("Specialist works 40h."); }
+    @Override public void eat() { System.out.println("Eating at doctor's lounge."); }
+    @Override public void vacations() { System.out.println("30 days of vacation."); }
+
+    public void specialtyMethod() { System.out.println("Performing " + specialty + " consultation."); }
+}
