57a7e13a6e702e3bf8e8a90b86738e6a33fa32ac
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-02 18:06:20 +0100

    feat: IStandardCare.java

---
+++ src/main/java/org/example/Characters.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public abstract class Characters {
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public Characters(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and Setters (Encapsulation)
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
---
+++ src/main/java/org/example/IStandardCare.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface IStandardCare {
+    void admission();
+    void nurseControl();
+    void room();
+    void treatment();
+    void nextAppointment();
+    void discharge();
+}
