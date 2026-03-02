4032b6ee7a791478f672e8896e2622d37648d338
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-03-02 18:11:32 +0100

    feat:Patient.java

---
+++ src/main/java/org/example/Patient.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public abstract class Patient extends Characters {
+    public Patient(String id, String firstName, String lastName) {
+        super(id, firstName, lastName);
+    }
+}
